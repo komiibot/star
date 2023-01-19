6a5f64707b026adc7f098826093c0918489d3d8e
Keira C <109384098+keiracodes@users.noreply.github.com>
2023-01-19 00:08:20 -0400

    Update blacklist.ts

--- src/commands/developer/blacklist.ts
+++ src/commands/developer/blacklist.ts
@@ -6,7 +6,7 @@ export default commandModule({
 	description: "Blacklist a user, or guild",
 	alias: ["bl"],
 	execute: async (ctx, args) => {
-		if (!["583925649807245322", "434008547139911681", "1048860807842234469", "199801459469058048"].includes(ctx.user.id)) return;
+		if (!["583925649807245322", "1048860807842234469", "199801459469058048"].includes(ctx.user.id)) return;
 
         // ctx.channel.send("1063684564750442598", "")
     }
